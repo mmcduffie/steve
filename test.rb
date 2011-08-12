2555772fb0701c4f789480f4781934c683b7ae63
Marcus McDuffie <whitehatpsycho@gmail.com>
2011-08-12 10:35:37 -0700

    hummm...

--- test.rb
+++ test.rb
@@ -1,7 +1,7 @@
 source = <<-eos
   [    []
      
-       [   []     ]     ]
+       [   []     []     ]
      
        eos
        
@@ -10,15 +10,18 @@ chars = source.split(//)
 $mem = []
 $stack_level = 0
 
-def test_char(char)
+def test_char(char,pos)
   if char =~ /\[/
     $mem.push([])
     $stack_level += 1
+  elsif char =~ /\]/
+    $stack_level -= 1
   end
 end
 
-chars.each do |c|
-  test_char(c)
+chars.each_with_index do |c,i|
+  test_char(c,i)
 end
 
 puts $mem.inspect
+puts $stack_level
