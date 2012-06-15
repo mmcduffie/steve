358f0e65b4ecc42ecc6d189f675a58056c27ef4d
Marcus McDuffie <whitehatpsycho@gmail.com>
2012-06-14 23:08:30 -0400

    finally got the new parser working good... I think.

--- lib/steve/new_parser.rb
+++ lib/steve/new_parser.rb
@@ -1,17 +1,16 @@
 module Steve
   class NewParser
     attr_accessor :parser_stack
-    attr_accessor :lookahead_token
     attr_accessor :input_tokens
     def initialize(grammar_rules,input_tokens)
       @grammar_rules = grammar_rules
-      @input_tokens = input_tokens
+      @input_tokens = input_tokens.reverse!
       @parser_stack = []
-      @lookahead_token = @input_tokens.shift
     end
     def shift
-      @parser_stack.push @lookahead_token
-      @lookahead_token = @input_tokens.shift
+      unless @input_tokens.empty?
+        @parser_stack.push @input_tokens.pop
+      end
     end
     def reduce(name,root)
       reduced_token = Steve::Token.new name, @parser_stack, root, [], []
@@ -20,11 +19,6 @@ module Steve
     end
     def match(stack)
       @grammar_rules.each do |rule|
-        #puts "\n"
-        #puts "stack: #{stack}"
-        #puts "\n"
-        #puts "grammar_rules: #{rule.components}"
-        #puts "\n"
         if stack == rule.components
           return rule
         else
@@ -33,33 +27,22 @@ module Steve
       end
     end
     def parse
-      #puts "start: #{@input_tokens.inspect}"
       if @input_tokens.length == 1 && @input_tokens[0].root
-        return @input_tokens
+        return @input_tokens[0]
       end
-      # Create an array of tokens that includes the lookahead token.
-      comparison_stack = @parser_stack
-      comparison_stack.push @lookahead_token
-      # Try to match using the lookahead token.
-      match_result = match(comparison_stack)
-      #puts "Match with lookahead: #{match_result}"
-      if match_result
-        shift
-        parse
-      end
-      #puts "Lookahead: #{@input_tokens.inspect}"
-      # Try to match usin the parser stack.
-      match_result = match(@parser_stack)
-      #puts "Match without lookahead: #{match_result}"
-      if match_result
-        reduce match_result.name, match_result.root
-        shift
-        parse
-      end
-      #puts "Stack: #{@input_tokens.inspect}"
-      # If no match, shift another token.
       shift
-      # Recursively call parse another time.
+      stack_with_lookahead = @parser_stack
+      stack_with_lookahead.push @input_tokens.last
+      lookahead_match = match stack_with_lookahead
+      if lookahead_match
+        shift
+        reduce lookahead_match.name, lookahead_match.root
+      else
+        match = match @parser_stack
+        if match
+          reduce match.name, match.root
+        end
+      end
       parse
     end
   end
--- test/test_new_parser.rb
+++ test/test_new_parser.rb
@@ -4,22 +4,22 @@ class NewParserTest < Test::Unit::TestCase
   def test_shift
     parser = Steve::NewParser.new [], []
     test_token = Steve::Token.new "FOO", "foo", false, [], []
-    parser.lookahead_token = test_token
+    parser.input_tokens.push test_token
     parser.shift
-    #assert_equal [test_token], parser.parser_stack, "Shift did not happen as we expected."
+    assert_equal [test_token], parser.parser_stack, "Shift did not happen as we expected."
   end
   def test_multiple_shift
     parser = Steve::NewParser.new [], []
     token_1 = Steve::Token.new "FOO", "foo", false, [], []
     token_2 = Steve::Token.new "BAR", "bar", false, [], []
-    parser.lookahead_token = token_1
+    parser.input_tokens.push token_1
     parser.shift
-    parser.lookahead_token = token_2
+    parser.input_tokens.push token_2
     parser.shift
-    #assert_equal [
-    #  token_1,
-    #  token_2
-    #], parser.parser_stack, "Shift did not happen as we expected, parser stack needs to stay in correct order."
+    assert_equal [
+      token_1,
+      token_2
+    ], parser.parser_stack, "Shift did not happen as we expected, parser stack needs to stay in correct order."
   end
   def test_reduce
     parser = Steve::NewParser.new [], []
@@ -38,7 +38,7 @@ class NewParserTest < Test::Unit::TestCase
     ]
 
     parser.reduce "ROOT", true
-    #assert_equal [root_token], parser.input_tokens, "Reduction did not happen as we expected."
+    assert_equal [root_token], parser.input_tokens, "Reduction did not happen as we expected."
   end
   def test_match
     symbol_component1 = Steve::Token.new "BAR", "bar", false, [], []
@@ -56,7 +56,7 @@ class NewParserTest < Test::Unit::TestCase
 
     parser = Steve::NewParser.new [symbol], []
 
-    #assert_equal symbol, parser.match(parser_stack), "Match did not return the name of the matching non-terminal."
+    assert_equal symbol, parser.match(parser_stack), "Match did not return the name of the matching non-terminal."
   end
   def test_simple_parse
     symbol_component1 = Steve::Token.new "BAR", "bar", false, [], []
@@ -75,6 +75,36 @@ class NewParserTest < Test::Unit::TestCase
     parser = Steve::NewParser.new [symbol], [
       token1,
       token2
+    ]
+
+    assert_equal root_token, parser.parse, "Parse did not occur properly."
+  end
+  def test_parse_with_lookahead
+    symbol_component1 = Steve::Token.new "BAR", "bar", false, [], []
+    symbol_component2 = Steve::Token.new "BAR", "bar", false, [], []
+
+    symbol_1 = Steve::Token.new "BARS", "", false, [symbol_component1,symbol_component2], []
+
+    symbol_component3 = Steve::Token.new "BAR", "bar", false, [], []
+    symbol_component4 = Steve::Token.new "BAR", "bar", false, [], []
+    symbol_component5 = Steve::Token.new "FOO", "foo", false, [], []
+
+    symbol_2 = Steve::Token.new "ROOT", "", true, [symbol_component3,symbol_component4,symbol_component5], []
+
+    token1 = Steve::Token.new "BAR", "bar", false, [], []
+    token2 = Steve::Token.new "BAR", "bar", false, [], []
+    token3 = Steve::Token.new "FOO", "foo", false, [], []
+
+    root_token = Steve::Token.new "ROOT", [
+      token1,
+      token2,
+      token3
+    ], true, [], []
+
+    parser = Steve::NewParser.new [symbol_1,symbol_2], [
+      token1,
+      token2,
+      token3
     ]
 
     #assert_equal root_token, parser.parse, "Parse did not occur properly."
